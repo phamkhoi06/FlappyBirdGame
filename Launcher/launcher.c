c7ba04961c20fca00be35b2b7236e0781139ecde
khoipm06 <62430582+phamkhoi06@users.noreply.github.com>
2025-04-02 08:17:45 +0700

    feat: Add launcher project for massiv polluted DLLs

---
+++ Launcher/launcher.c
@@ -0,0 +1,36 @@
+#include <windows.h>
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+    STARTUPINFO si = { 0 };
+    PROCESS_INFORMATION pi = { 0 };
+    si.cb = sizeof(si);
+
+    LPCWSTR exeName = L"bin\\FlappyBirdGame.exe";
+    LPCWSTR workingDir = NULL;
+
+    // Create the process
+    if (!CreateProcess(
+        exeName,   // Application name
+        NULL,      // Command line arguments
+        NULL,      // Process handle not inheritable
+        NULL,      // Thread handle not inheritable
+        FALSE,     // Set handle inheritance to FALSE
+        0,         // No creation flags
+        NULL,      // Use parent's environment block
+        workingDir,// Set working directory
+        &si,       // Pointer to STARTUPINFO structure
+        &pi)) {    // Pointer to PROCESS_INFORMATION structure
+        MessageBox(NULL, L"Failed to start FlappyBirdGame.exe", L"Error", MB_OK | MB_ICONERROR);
+        return 1;
+    }
+
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return 0;
+}
